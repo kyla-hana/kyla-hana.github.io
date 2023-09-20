9091fe1101e41d296b7bc687d32f27e2167d508d
Hana J <kjones25@livingschoolnola.org>
2023-09-20 20:35:04 +0000

    pink

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,7 +43,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+createPlatform(300,630,100,10);
+createPlatform 
 
 
     
